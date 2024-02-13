421c92f8a6d607d468cb7197c9dab108e548f205
Mohamed Aziz Tousli <mohamedaziztousli@gmail.com>
2024-02-13 22:16:04 +0100

    Runtime: 32 ms (81.48%), Memory: 16.6 MB (53.75%) - Gitcode

---
+++ 191-number-of-1-bits/191. Number of 1 Bits.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+        nb = 0
+        for i in range(33):
+            nb = 2 ** i
+            # print(i, nb)
+            temp = n & nb
+            if temp != 0:
+                res += 1
+        # print(str(n))
+        return res
