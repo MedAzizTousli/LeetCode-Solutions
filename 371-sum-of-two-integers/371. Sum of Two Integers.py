d10a17ded8c8708117ee171accaf999071e010e0
Mohamed Aziz Tousli <mohamedaziztousli@gmail.com>
2024-02-19 22:09:31 +0100

    Runtime: 35 ms (60.02%), Memory: 16.6 MB (55.03%) - Gitcode

--- 371-sum-of-two-integers/371. Sum of Two Integers.py
+++ 371-sum-of-two-integers/371. Sum of Two Integers.py
@@ -1,16 +1,13 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        def add(a, b):
-            if not a or not b:
-                return a or b
-            return add(a ^ b, (a & b) << 1)
+        mask = 0xffffffff
 
-        if a * b < 0:  # assume a < 0, b > 0
-            if a > 0:
-                return self.getSum(b, a)
-            if add(~a, 1) == b:  # -a == b
-                return 0
-            if add(~a, 1) < b:  # -a < b
-                return add(~add(add(~a, 1), add(~b, 1)), 1)  # -add(-a, -b)
+        while b != 0:
+            tmp = (a & b) << 1
+            a = (a ^ b) & mask
+            b = tmp & mask
 
-        return add(a, b)  # a*b >= 0 or (-a) > b > 0
+        if a > mask // 2:
+            return ~(a ^ mask)
+        else:
+            return a
