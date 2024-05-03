51db62477017f25bbc5441957516cae669c62152
Mohamed Aziz Tousli <mohamedaziztousli@gmail.com>
2024-05-03 22:18:59 +0100

    Runtime: 94 ms (92.96%), Memory: 17.5 MB (28.77%) - Gitcode

---
+++ 73-set-matrix-zeroes/73. Set Matrix Zeroes.py
@@ -0,0 +1,16 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        R, C = len(matrix), len(matrix[0])
+        rows = set()
+        cols = set()
+        for i in range(R):
+            for j in range(C):
+                if matrix[i][j] == 0:
+                    rows.add(i)
+                    cols.add(j)
+        print(rows)
+        print(cols)
+        for i in range(R):
+            for j in range(C):
+                if i in rows or j in cols:
+                    matrix[i][j] = 0
