996019a4ee08a0612b6fddf920d227dfc4560e48
Mohamed Aziz Tousli <mohamedaziztousli@gmail.com>
2024-05-07 22:00:18 +0100

    Runtime: 68 ms (91.43%), Memory: 17.9 MB (5.14%) - Gitcode

--- 332-reconstruct-itinerary/332. Reconstruct Itinerary.py
+++ 332-reconstruct-itinerary/332. Reconstruct Itinerary.py
@@ -3,18 +3,34 @@ class Solution:
         self.res = []
 
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        visited = defaultdict(int)
         mp = defaultdict(list)
+        cnt = defaultdict(int)
+
         for i in tickets:
             mp[i[0]].append(i[1])
+            cnt[i[1]] += 1
         for i in mp:
             mp[i].sort(reverse=True)
-
+        cnt["JFK"] += 1
+        visited = set()
+        print(cnt)
+        print(mp)
+        print(visited)
         def dfs(curr):
+            # if curr in visited:
+            #     return
+            # visited[curr] += 1
+            # visited.add(curr)
             st = mp[curr]
             while st != []:
                 visit = st.pop()
                 dfs(visit)
             self.res.append(curr)
+            # for i in mp[curr]:
+            #     dfs(i)
+            # visited.remove(curr)
+            # visited[curr] -= 1
 
         dfs("JFK")
         return reversed(self.res)
